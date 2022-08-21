d3884008d2f7901d165bebb8cb763503d7c94c50
Thomas Galliker <thomas@bluewin.ch>
2022-08-21 12:12:37 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# OpenWeatherMap.API
- OpenWeatherMap API client for .NET
+# OpenWeatherMap
+[![Version](https://img.shields.io/nuget/v/OpenWeatherMap.API.svg)](https://www.nuget.org/packages/OpenWeatherMap.API)  [![Downloads](https://img.shields.io/nuget/dt/OpenWeatherMap.API.svg)](https://www.nuget.org/packages/OpenWeatherMap.API)
+
+OpenWeatherMap API client for .NET
+
+### Download and Install OpenWeatherMap
+This library is available on NuGet: https://www.nuget.org/packages/OpenWeatherMap.API
+Use the following command to install OpenWeatherMap.API using NuGet package manager console:
+
+    PM> Install-Package OpenWeatherMap.API
+
+You can use this library in any .NET project which is compatible to .NET Standard 2.0 and higher.
+
+### API Usage
+The following sections document basic use cases of this library. The following code excerpts can also be found in the [sample applications](https://github.com/thomasgalliker/OpenWeatherMap.API/tree/develop/Samples).
+
+#### Request weather info by PLZ
+`MeteoSwissWeatherService` is the main entry point of this library. Create an instance of `MeteoSwissWeatherService` or inject `IMeteoSwissWeatherService` using dependency injection techniques.
+```C#
+IMeteoSwissWeatherService weatherService = new MeteoSwissWeatherService(logger, weatherServiceConfiguration);
+```
+Request weather information for any Swiss zip code (plz):
+```C#
+var weatherInfo = await weatherService.GetCurrentWeatherAsync(plz: 6330);
+```
+
+### Links
+Federal Office of Meteorology and Climatology MeteoSwiss
+https://www.meteoschweiz.admin.ch
+
+### Contribution
+Contributors welcome! If you find a bug or you want to propose a new feature, feel free to do so by opening a new issue on github.com.
+
+### License
+Check the terms and conditions of the Federal Office of Meteorology and Climatology MeteoSwiss before using this client library.
---
+++ Samples/OpenWeatherMap.ConsoleSample/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace OpenWeatherMap.ConsoleSample
+{
+    public static class Program
+    {
+        private static IConfigurationRoot configuration;
+
+        private static async Task Main(string[] args)
+        {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+            Console.WriteLine($"OpenWeatherMap.ConsoleSample [Version 1.0.0.0]");
+            Console.WriteLine($"(c) 2022 superdev gmbh. All rights reserved.");
+            Console.WriteLine();
+
+            configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+
+            using var loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder.AddConsole();
+            });
+
+            //// Create weather service instance manually or resolve it from any dependency injection framework:
+            //var logger = loggerFactory.CreateLogger<MeteoSwissWeatherService>();
+            //IMeteoSwissWeatherServiceConfiguration weatherServiceConfiguration = new MeteoSwissWeatherServiceConfiguration();
+            //IMeteoSwissWeatherService weatherService = new MeteoSwissWeatherService(logger, weatherServiceConfiguration);
+
+            //// Request weather info:
+            //var weatherInfo = await weatherService.GetCurrentWeatherAsync(plz: 6330);
+
+            //Console.WriteLine();
+            //Console.WriteLine(ObjectDumper.Dump(weatherInfo.CurrentWeather));
+
+            //Console.WriteLine();
+            //Console.WriteLine(ObjectDumper.Dump(weatherInfo.Forecast));
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to close this window...");
+            Console.ReadKey();
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Console.WriteLine($"{e.ExceptionObject}");
+        }
+    }
+}
