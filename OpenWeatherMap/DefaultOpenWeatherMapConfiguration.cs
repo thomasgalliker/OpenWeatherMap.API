5546461a93432b68bee360438eefd7aa42370a6c
Thomas Galliker <thomas@bluewin.ch>
2023-04-22 11:10:33 +0200

    Add new default config

---
+++ OpenWeatherMap/DefaultOpenWeatherMapConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace OpenWeatherMap
+{
+    public class DefaultOpenWeatherMapConfiguration : OpenWeatherMapConfiguration
+    {
+        public DefaultOpenWeatherMapConfiguration(string apiKey)
+        {
+            this.ApiEndpoint = "https://api.openweathermap.org";
+            this.ApiKey = apiKey;
+            this.UnitSystem = "metric";
+            this.Language = "en";
+        }
+    }
+}
